2eb627b976b985a7fe05820eb4eafee30bb271b2
shugo256 <shugo256@gmail.com>
2019-05-02 21:16:31 +0900

    Exawizards C

--- others/Exawizards_2019_C_Snuk_the_Wizard2.cpp
+++ others/Exawizards_2019_C_Snuk_the_Wizard2.cpp
@@ -15,7 +15,7 @@ P spels[MAX+1];
 
 int journey(int k) {
 	for (int i=0; i<q; i++) {
-		if (s[k] != spels[i].first) continue;
+		if (s[(size_t)k] != spels[i].first) continue;
 		k += spels[i].second;
 		if (k == n) return 1;
 		else if (k == -1) return -1;
@@ -31,15 +31,16 @@ int main() {
 		spels[i] = P(t, d == 'R' ? 1 : -1);
 	}
 
-	int a1 = 0, a2 = 0, b1 = s.size()-1, b2 = s.size()-1, j1, j2;
-	while (b1 - a1 > 1 && b2 - a2 > 1) {
+	int a1 = 0, a2 = 0, b1 = n-1, b2 = n-1, j1, j2;
+	while (true) {
 		j1 = journey((a1 + b1)/2);
 		j2 = journey((a2 + b2 + 1)/2);
+		if (b1 - a1 <= 1 && b2 - a2 <= 1) break;
 		if (j1 == -1) a1 = (a1 + b1)/2;
 		else b1 = (a1 + b1)/2;
 		if (j2 == 1) b2 = (a2 + b2 + 1)/2;
 		else a2 = (a2 + b2 + 1)/2;
 	}
-	cout << b2 - a1 + (j1 != -1 ? 1 : 0) + (j2 != 1 ? 1 : 0) - 2 << '\n';
+	cout << b2 - a1 + (int)(j1==0) + (int)(j2==0) - 1 << '\n';
 	return 0;
 }
