b960fbea7178a618ccb5bd0637bdacf647cc84e1
shugo256 <shugo256@gmail.com>
2020-01-31 13:01:29 +0900

    fixing git ignore

--- ABC/114_A_753.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-
-int main() {
-    int X;
-    scanf("%d", &X);
-    printf("%s\n", X == 3 || X == 5 || X == 7 ? "YES" : "NO");
-    return 0;
-}
--- others/CADDI_2018_A.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main() {
-    int N, count=0;
-    scanf("%d",&N);
-    for ( ; N > 0; N /= 10) if (N % 10 == 2) count++;
-    printf("%d\n",count);
-    return 0;
-}
